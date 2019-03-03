1f86d89d224d89b67e867dcb2c6eadc51e2f571a
Salikh <salikh.sayfutdinov@gmail.com>
2019-03-03 15:42:05 +0500

    Создан обработчик пост запросов

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+var arr = ['hello', 'world', 'test'];
+
+app.get('/', function (req, res) {
+    res.render('index', {arr: arr});
+});
+
+app.get('/create', function (req, res) {
+    res.render('create');
+});
+
+app.post('/create', function (req, res) {
+    console.log(req.body);
+    arr.push(req.body.text);
+    res.redirect('/');
+});
+
+app.listen(3000, () => console.log('Слушаем порт 3000'));
