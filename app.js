631ad48b3f7416988a03d0bd721e3ee651e87f48
Salikh Sayfutdinov <salikh.sayfutdinov@gmail.com>
2019-03-04 17:17:53 +0500

    Блог заработал

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 const express = require('express');
-
 const bodyParser = require('body-parser');
 
+const Post = require('./models/post');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
-var arr = ['hello', 'world', 'test'];
-
-app.get('/', function (req, res) {
-    res.render('index', {arr: arr});
+app.get('/',(req, res) => {
+    Post.find({}).then(posts => {
+        res.render('index', {posts: posts});
+    })
 });
+
+// app.get('/', function (req, res) {
+//     res.render('index', {arr: arr});
+// });
 
 app.get('/create', function (req, res) {
     res.render('create');
 });
 
 app.post('/create', function (req, res) {
-    console.log(req.body);
-    arr.push(req.body.text);
+    const {title, body} = req.body;
+    Post.create({
+        title: title,
+        body: body
+    }).then(post => console.log(post._id));
     res.redirect('/');
 });
 
---
+++ models/post.js
@@ -0,0 +1,17 @@
+const mongoose =require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String
+    }
+})
+
+module.exports = mongoose.model('Post', schema);
+
+
+
