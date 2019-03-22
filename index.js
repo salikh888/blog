468959a864383fd54181a17c79de311fafc39e29
Salikh Sayfutdinov <salikh.sayfutdinov@gmail.com>
2019-03-22 18:11:28 +0500

    Добавили верстку

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = require('./app');
 const database = require('./database');
 const config = require('./config');
 
+
 database().then(info => {
     console.log(`Connected to ${info.host}:${info.port}/${info.name}`);
     app.listen(config.PORT, function () {
