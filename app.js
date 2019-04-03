7a6c0c4dec41571911203b259e367142ec1422cd
Salikh Sayfutdinov <salikh.sayfutdinov@gmail.com>
2019-04-03 11:28:45 +0500

    поключение к монге перенес в app.js

--- app.js
+++ app.js
@@ -2,9 +2,27 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const staticAsset = require('static-asset');
+const mongoose = require('mongoose');
+const config = require('./config');
 
+//database
+mongoose.Promise = global.Promise;
+mongoose.set('debug', config.IS_PRODUCTION);
+mongoose.connection
+    .on('error', error => console.log(error))
+    .on('error', function(){
+        console.log('Database connection closed.')
+    })
+    .once('open', function () {
+        const info = mongoose.connections[0];
+        console.log(`Connected to ${info.host}:${info.port}/${info.name}`);
+    });
+mongoose.connect(config.MONGO_URL, {useNewUrlParser: true});
+
+//express
 const app = express();
 
+//set and uses
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(staticAsset(path.join(__dirname, 'public')))
@@ -14,8 +32,11 @@ app.use(
     express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist'))
 );
 
+//routers
 app.get('/',(req, res) => {
     res.render('index');
 });
 
-module.exports = app;
+app.listen(config.PORT, function () {
+    console.log('Слушаем порт ' + config.PORT);
+});
